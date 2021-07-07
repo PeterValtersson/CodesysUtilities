22c7a0f315abb5eeb1d09e25ac200de314294aa7
Peter <peter.walter.valtersson@gmail.com>
2021-07-07 07:36:21 +0200

    Fixed copy ,py

--- setup.bat
+++ setup.bat
@@ -1,8 +1,8 @@
 
-set destination="C:\ProgramData\CODESYS\Script Commands"
+set destination=C:\ProgramData\CODESYS\Script Commands
 
-if not exist %destination%\NUL mkdir %destination%
+if not exist "%destination%" mkdir "%destination%"
 
-copy "config.json" %destination%\config.json
-copy "CoDeSys.ico" %destination%\CoDeSys.ico
-for . %%f in (*.py) do copy %%f %destination%
+copy "config.json" "%destination%\config.json"
+copy "CoDeSys.ico" "%destination%\CoDeSys.ico"
+xcopy "%cd%\*.py" "%destination%" /y
