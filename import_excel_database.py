49fa3cba11ffaee922f777d57b89d73c0b6c3570
Peter Valtersson <peter.walter.valtersson@gmail.com>
2025-01-15 12:03:01 +0100

    Fixed problem with newlines in comment

--- import_excel_database.py
+++ import_excel_database.py
@@ -140,53 +140,6 @@ class Excel:
 def raise_by_number_of_decimals(value, decimals):
     return int(float(value)*pow(10, int(decimals)))
 
-class DatabaseEntryData:
-    def __init__(self, excluded, type, id, name):
-        self.excluded = excluded
-        self.type = type
-        self.id = id
-        self.name = name
-
-class DigitalInputOutput(DatabaseEntryData):
-    def __init__(self, init_database_method_string, excluded, type, id, name, channel, pin, location):
-        DatabaseEntryData.__init__(self, excluded, type, id, name)
-        self.init_database_method_string = init_database_method_string
-        self.channel = channel
-        self.pin = pin
-        self.location = location
-        self.comment = "{}, Channel: {}, Pin: {}, Location: {}".format(type, channel, pin, location)
-
-    def get_init_database_string(self):
-        return "{}(DatabaseID.{}, epirocTypes.Location.{}, {}, {});\n".format(
-            self.init_database_method_string,
-            DatabaseNameFormater.format(self.name), 
-            self.location, 
-            self.channel, 
-            self.pin)
-
-class DigitalOutput(DigitalInputOutput):
-    def __init__(self, excluded, id, name, channel, pin, location):
-        DatabaseEntryData.__init__(self, "init_digital_output", excluded, "PhysicalDigitalOutput", id, name, channel, pin, location)
-    
-class Parameter(DatabaseEntryData):
-    def __init__(self, id, name, value, max_value, min_value, num_dec, unit, comment):
-        DatabaseEntryData.__init__(self, False, "Parameter", id, name)
-        self.value = raise_by_number_of_decimals(value, num_dec)
-        self.max_value = raise_by_number_of_decimals(max_value, num_dec)
-        self.min_value = raise_by_number_of_decimals(min_value, num_dec)
-        self.num_dec = int(num_dec)
-        self.unit = unit
-        self.comment = "{}, Default: {}, Max: {}, Min: {}, Num Decimals: {}, Unit: {}".format(comment, value, max_value, min_value, int(num_dec), unit)
-
-    def get_init_database_string(self):
-        return "init_parameter(DatabaseID.{}, {}, {}, {}, {}, epirocTypes.Units.{});\n".format(
-            DatabaseNameFormater.format(self.name), 
-            self.value, 
-            self.max_value, 
-            self.min_value, 
-            self.num_dec, 
-            self.unit)
-
 class WorksheetExtractor:
     @staticmethod
     def extract(worksheet, num_entries):
@@ -256,75 +209,6 @@ class DatabaseEntryExtractorBase:
         del column[0]
         return column
 
-
-class ParameterExtractor(DatabaseEntryExtractorBase): 
-    def __init__(self, worksheet):
-        DatabaseEntryExtractorBase.__init__(self, worksheet)
-
-    def extract_all_parameters(self):
-        data_arrays = self._get_all_parameter_data_arrays()
-        return self._create_parameters_from_data_arrays(data_arrays)
-        
-    def _get_all_parameter_data_arrays(self):
-        class DataArrays(object): pass
-        data_arrays = DataArrays()
-        data_arrays.names = self._get_data_array("name")
-        data_arrays.values = self._get_data_array("value")
-        data_arrays.max_values = self._get_data_array("max_value")
-        data_arrays.min_values= self._get_data_array("min_value")
-        data_arrays.num_decs = self._get_data_array("num_dec")
-        data_arrays.units = self._get_data_array("unit")
-        data_arrays.comments = self._get_data_array("Comment")   
-        return data_arrays
-    
-    def _create_parameters_from_data_arrays(self, data_arrays):
-        parameters = []
-        for id, name in enumerate(data_arrays.names):
-            parameter = Parameter(id, name, data_arrays.values[id], data_arrays.max_values[id], data_arrays.min_values[id], data_arrays.num_decs[id], data_arrays.units[id], data_arrays.comments[id])
-            parameters.append(parameter)
-
-        return parameters
-
-
-class DigitalOutputExtractor(DatabaseEntryExtractorBase):      
-    def __init__(self, worksheet):
-        DatabaseEntryExtractorBase.__init__(self, worksheet)
-          
-    def extract_all_digial_outputs(self):
-        data_arrays = self._get_all_digial_output_data_arrays()
-        return self._create_digial_output_from_data_arrays(data_arrays)
- 
-    def _get_all_digial_output_data_arrays(self):
-        class DataArrays(object): pass
-        data_arrays = DataArrays()
-        data_arrays.excluded = self._get_data_array("Excluded:")
-        data_arrays.names = self._get_data_array("name")
-        data_arrays.channel = self._get_data_array("channel")
-        data_arrays.pin = self._get_data_array("pin")
-        data_arrays.location = self._get_data_array("location")
-        return data_arrays
-
-    def _create_digial_outputs_from_data_arrays(self, data_arrays):
-        entries = []
-        for id, name in enumerate(data_arrays.names):
-            entry = DigitalOutput(data_arrays.excluded[id] != "", id, name, data_arrays.channel[id], data_arrays.pin[id], data_arrays.location[id])
-            entries.append(entry)
-       
-        return entries
-
-class DatabaseExtractor:
-    @staticmethod
-    def extract(workbook):
-        class Entries(object): pass
-        entries = Entries()
-        entries.worksheets = workbook.get_worksheets()
-        print("Found {} worksheets".format(len(entries.worksheets)))
-        for ws in entries.worksheets:
-            print(ws.get_name())
-        entries.parameters = ParameterExtractor(workbook.get_worksheet_by_name("Parameters")).extract_all_parameters()
-        #entries.digital_outputs = DigitalOutputExtractor(workbook.get_worksheet_by_name("OutputsDig")).extract_all_digial_outputs()
-        return entries
-
 class DatabaseNameFormater:
     @staticmethod
     def format(name):
@@ -337,13 +221,16 @@ class CommentFormater:
         if type is not None:
             to_add.append(("", type))
         if "Comment" in sheet:
-            to_add.append(("", sheet["Comment"][entry]))
+            to_add.append(("", CommentFormater.clean_comment_string(sheet["Comment"][entry])))
         for c in ["value", "channel", "pin", "location", "num_dec", "max_value", "min_value", "unit", "max_actual", "min_actual", "unit_actual", "KP", "KI", "frequency", "dither_freq", "dither_value", "byte 3", "byte 2", "byte 1", "byte 0"]:
             if c in sheet:
                 to_add.append(("{}:".format(c), sheet[c][entry]))
         
         return ", ".join(["{} {}".format(t, v) for t, v in to_add])
-    
+    @staticmethod
+    def clean_comment_string(text):
+        return text.replace("\n", ": ")
+
 class DatabaseIDFormater:
     @staticmethod
     def format_all(sheets):
@@ -481,12 +368,12 @@ class TextlistFormater:
         translations = []
         for lan in languages:
             if lan in to_format:
-                translations.append((lan, to_format[lan][entry]))
+                translations.append((lan, CommentFormater.clean_comment_string(to_format[lan][entry])))
         return translations
     
     @staticmethod
     def format_comment(type, to_format):
-        return [(TextlistFormater.format_textid(type, i), s, TextlistFormater.format_translations(to_format, i))
+        return [(TextlistFormater.format_textid(type, i), CommentFormater.clean_comment_string(s), TextlistFormater.format_translations(to_format, i))
             for i, s in enumerate(to_format["Comment"])]
     
 
